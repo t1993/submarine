adc0aa288bbbaa76cb397719731314f31bd17a57
17600244825 <17600244825@163.com>
2023-12-01 09:54:46 +0800

    first

---
+++ submarine/src/test/java/com/simulator/submarine/SubmarineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simulator.submarine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubmarineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
